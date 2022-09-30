d6ff74d5d6901c7f8dca5f74eb4ad834f2102e89
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-09-30 10:29:41 -0400

    started adding prompts and README structure to index.js

---
+++ README.md
@@ -0,0 +1,22 @@
+# d
+    
+## Description
+    d
+    
+## Table of Contents
+    - [Installation Instructions](##installation-instructions)
+    - [Usage](##usage)
+    - [Contributing](##contributing)
+    - [Tests](##tests)
+    
+## Installation Instructions
+    d
+    
+## Usage
+    d
+    
+## Contributing
+    d
+    
+## Tests
+    d
--- index.js
+++ index.js
@@ -7,16 +7,50 @@ inquirer.prompt ([
         name: 'projectName',
         message: 'What is the name of the project?'
     },
+    {   
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of you project.'
+    },
     {
         type: 'input',
         name: 'installation',
         message: 'Enter any installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter any usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contributing'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter any testing information.'
     }
 ])
 .then((data) => {
     fs.writeFile('README.md',
-    `# ${projectName}
-    ## Installation Instructions
-    ${installation}`
+    `# ${data.projectName}
+    \n## Description
+    ${data.description}
+    \n## Table of Contents
+    - [Installation Instructions](##installation-instructions)
+    - [Usage](##usage)
+    - [Contributing](##contributing)
+    - [Tests](##tests)
+    \n## Installation Instructions
+    ${data.installation}
+    \n## Usage
+    ${data.usage}
+    \n## Contributing
+    ${data.contribution}
+    \n## Tests
+    ${data.tests}`, 
+    (err) => err ? console.log(err) : console.log('README.md created!')
     )
 })
